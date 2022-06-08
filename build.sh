bad01095ee3f31cd84e2568e847169b635ca128a
me <me>
2022-06-08 10:47:52 +0100

    docker changes

---
+++ build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+git clone git@github.com:hskingr/tfl_cams_maps_react_leaflet.git
+cd tfl_cams_maps_react_leaflet && docker compose build
+git clone git@github.com:hskingr/tfl_cams_maps_api.git
+cd tfl_cams_maps_api && docker compose build
+cd .. && docker compose up -d
