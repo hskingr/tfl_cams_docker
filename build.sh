a9d3689644509e814a5394fd3bf30334cbe4d1a1
me <me>
2022-06-08 11:04:25 +0100

    docker changes

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
 git clone git@github.com:hskingr/tfl_cams_maps_react_leaflet.git
-cd tfl_cams_maps_react_leaflet && docker compose build
+cd tfl_cams_maps_react_leaflet
+ls ./
+docker compose build
 git clone git@github.com:hskingr/tfl_cams_maps_api.git
-cd tfl_cams_maps_api && docker compose build
+cd tfl_cams_maps_api
+ls ./
+ docker compose build
 cd .. && docker compose up -d
